2e868531a8950d253fac37f85a0c041abeaa6772
Horvath Balazs <balazs.horvath989@gmail.com>
2017-09-30 12:31:35 +0200

    ingredients can be added to shopping list

--- recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addItem = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem(itemName: HTMLInputElement, itemAmount: HTMLInputElement) {
+    this.addItem.emit(new Ingredient(itemName.value, parseInt(itemAmount.value)));
   }
 
 }
--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addToList(item: Ingredient) {
+    this.ingredients.push(item);
+  }
+
 }
